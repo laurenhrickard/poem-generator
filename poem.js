18bc892fc3394449f374210b11d76ab2531952c0
laurenrickard13 <153468324+laurenhrickard@users.noreply.github.com>
2024-02-22 17:15:04 -0800

    Added JavaScript to search form and typewriter plugin for displaying results

--- poem.js
+++ poem.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: ["Poem line 1"],
+    autoStart: true,
+    delay: 4,
+    cursor: "",
+  });
+}
+
+let searchForm = document.querySelector("#poem-generator-form");
+searchForm.addEventListener("submit", generatePoem);
