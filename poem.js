42f12c0e5fe1f25426600aece61eb8bce107df0b
laurenrickard13 <153468324+laurenhrickard@users.noreply.github.com>
2024-02-22 18:01:31 -0800

    Added API integration to enable users to search for a poem prompt, injecting it into the HTML

--- poem.js
+++ poem.js
@@ -1,12 +1,26 @@
-function generatePoem(event) {
-  event.preventDefault();
+function displayPoem(response) {
+  let poemText = document.querySelector("#poem");
+  poemText.innerHTML = `${response.data.answer}`;
 
   new Typewriter("#poem", {
-    strings: ["Poem line 1"],
+    strings: response.data.answer,
     autoStart: true,
     delay: 4,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let searchInput = document.querySelector("#search-box");
+  let apiKey = "6a3d5679t1daboe944af11da0383dbcf";
+  let prompt = `generate a four line poem in English about ${searchInput.value}`;
+  let context =
+    "please deliver the poem in html, please only deliver the poem, no other words, and separate each line with a <br /> except the last one. please capitalize the first word of each line. Do not give us a title. Do not change the styling of the words, no italics or bold.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let searchForm = document.querySelector("#poem-generator-form");
